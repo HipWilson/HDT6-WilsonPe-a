23f50a9cb500ab5d21636d6570a2089e0925e125
HipWilson <pen24760@uvg.edu.gt>
2025-03-11 22:12:36 -0600

    Ultima prueba unitaria

---
+++ PokemonStreamManagerTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+
+public class PokemonStreamManagerTest {
+    @Test
+    public void test(){
+        PokemonStreamManagerTest pok = new PokemonStreamManagerTest(); 
+        Assert.assertEquals(pok, pok);
+    }
+}
